e71d22e6a933f2a85ba5b2cbe1d522c5a6993a8f
alexefimov2020 <72080934+alexefimov2020@users.noreply.github.com>
2020-12-10 09:47:22 +0400

    Add files via upload

---
+++ messages.py
@@ -0,0 +1,15 @@
+    # -*- coding: utf-8 -*-
+
+CONNECTION_ERROR = "Could not connect to server"
+ERROR = "Error"
+INPUT_SERVER_HOST = "Input Server Host"
+INPUT_SERVER_PORT = "Input Server Port"
+INPUT_DEVICE = "Input number of device"
+INPUT_USERNAME = "Input your username"
+SEND = "Send"
+RECORD = "Record"
+STOP = "Stop"
+SERVER_HOST = "Server Host"
+SERVER_PORT = "Server Port"
+TITLE = "ezChat"
+USERNAME = "Username"
